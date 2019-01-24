3e43b4622dd9cf5f6a0ed71a041e62257ebc19d0
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-01-24 00:22:13 -0500

    Update sendgrid username and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
-    user_name: ENV['app112688816@heroku.com'],
-    password: ENV['gtwvfry18187'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
